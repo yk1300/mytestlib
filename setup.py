a1501b716c94435c8444a8ade4236cb4eaa5eb26
yk1300 <79400049+yk1300@users.noreply.github.com>
2021-02-21 19:00:56 +0900

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup,find_packages
+
+setup(
+      name='mytestlib-gon',
+      version='1.0.0',
+      url='',
+      author_email='',
+      description='My Test Library.',
+      packages=find_packages()
+)
+
